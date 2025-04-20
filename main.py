b93e360300a9f7b75b3402890dfccbfe1a2484e4
sonakshi <sonakshipuri06@gmail.com>
2025-04-20 19:21:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+    cipher=''
+    for letter in message:
+        if letter != ' ':
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            cipher+=''
+    return cipher
+
+
+def decrypt(message):
+    message += ' '
+    decipher = ''
+    citext = ''
+    for letter in message:
+        if (letter != ' '):
+            i = 0
+            citext += letter
+        else:
+            i += 1
+            if i == 2:
+                decipher += ' '
+            else:
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                                                              .values()).index(citext)]
+                citext = ''
+
+    return decipher
+
+action= input(' do you wanna encrypt or decrypt?')
+if action== 'encrypt':
+    message= input("what do you wanna encrypt?")
+    result= encrypt(message.upper())
+    print(result)
+
+if action=='decrypt':
+    message= input("what do you wanna decrypt?")
+    result= decrypt(message)
+    print(result)
